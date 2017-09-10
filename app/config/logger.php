4237073838db9dcebf17f6d3efa15d6119fe86d4
thanhdo <thanhdo610@gmail.com>
2017-09-10 22:12:28 +0700

    use full url for log folder

--- app/config/logger.php
+++ app/config/logger.php
@@ -3,4 +3,4 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 use Phalcon\Logger;
 use Phalcon\Logger\Adapter\File as FileAdapter;
 
-$logger = new FileAdapter(__DIR__ . '/../../log/' . date("Y-m-d") . '.log');
+$logger = new FileAdapter('/var/www/magiao_videoblog_api/log/' . date("Y-m-d") . '.log');
